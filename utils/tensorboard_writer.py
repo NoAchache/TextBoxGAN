07dd4f32f79eab4f711f37766d62e79f398acf9c
NoAchache <noe.achache@gmail.com>
2022-05-22 13:40:07 +0200

    log configs nicely

--- utils/tensorboard_writer.py
+++ utils/tensorboard_writer.py
@@ -36,9 +36,10 @@ class TensorboardWriter:
                 tf.summary.scalar(loss_name, metric.result(), step=step)
 
     def log_config_file(self, step: int) -> None:
-        json_configs = json.dumps(cfg, default=lambda o: str(o), indent=1)
+        """Log the configs as a table"""
+        configs = [tf.convert_to_tensor([k, str(v)]) for k, v in cfg.items()]
         with self.train_summary_writer.as_default():
-            tf.summary.text("configs", json_configs, step=step)
+            tf.summary.text("configs", tf.stack(configs), step=step)
 
     def log_images(
         self,
